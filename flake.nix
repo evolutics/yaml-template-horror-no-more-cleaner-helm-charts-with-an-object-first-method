27f037bd6b1f094ca76c8fd3415ef08942c92c0d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-07-20 10:13:22 +0200

    Check general code cleanliness in Nix environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+# Update-worthy: `flake.lock` file.
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    travel-kit.url = "github:evolutics/travel-kit";
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    travel-kit,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs =
+          (with pkgs; [
+            kubernetes-helm
+          ])
+          ++ [travel-kit.packages.${system}.default];
+      };
+    });
+}
